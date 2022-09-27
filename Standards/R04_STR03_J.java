1420c842f83c496bb5d5733982290c490dd7fc66
Perl-R <robinaaperl@gmail.com>
2022-09-27 16:07:55 -0400

    Create Rule 04 STR03 J

---
+++ Standards/R04_STR03_J.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class R04_STR03_J {
+ 
+    // https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string
+    // Rule 04. Charracters and Strings (STR)
+    // STR03-J. Do not encode noncharacter data as a string
+
+    public static void main(String[] args) {
+
+        BigInteger x = new BigInteger("530500452766");
+        String s = x.toString();  // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray); 
+        x = new BigInteger(ns);
+    
+    }
+}
